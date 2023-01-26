0d84489e391edfe908ea0a387697711d4dee231b
arthamevia <yuli45904@gmail.com>
2023-01-26 12:55:44 +0700

    GO-MOVIES

---
+++ app/Http/Controllers/StudioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Studio;
+use Illuminate\Http\Request;
+
+class StudioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Studio  $studio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Studio $studio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Studio  $studio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Studio $studio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Studio  $studio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Studio $studio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Studio  $studio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Studio $studio)
+    {
+        //
+    }
+}
--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
                 <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>T-MOVIE</h3>
+                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>GO-MOVIES</h3>
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
@@ -9,7 +9,7 @@
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0">T-Movie</h6>
+                        <h6 class="mb-0">GO-MOVIES</h6>
                         <span>Admin</span>
                     </div>
                 </div>
